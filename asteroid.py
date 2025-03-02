9ef799074d2bd78ca0180729bed874bf5e93c8d3
afiqyusof <afiqisking@gmail.com>
2025-03-02 21:21:05 +0800

    Base game of asteroids

---
+++ asteroid.py
@@ -0,0 +1,40 @@
+from circleshape import *
+from constants import *
+import random
+
+class Asteroid(CircleShape):
+
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+
+    def update(self, dt):
+        self.position += (self.velocity * dt) 
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            angle = random.uniform(20, 50)
+            vector1 = self.velocity.rotate(angle)
+            vector2 = self.velocity.rotate(-angle)
+            new_size = self.radius - ASTEROID_MIN_RADIUS
+            
+            asteroid_1 = Asteroid(self.position.x, self.position.y, new_size)
+            asteroid_2 = Asteroid(self.position.x, self.position.y, new_size)
+
+            asteroid_1.velocity = vector1 * 1.2
+            asteroid_2.velocity = vector2 * 1.2
+
+class Shot(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+
+    def update(self, dt):
+        self.position += (self.velocity * dt) 
--- main.py
+++ main.py
@@ -1,15 +1,62 @@
 from constants import *
+from player import *
+from asteroid import *
+from asteroidfield import *
 import pygame
+import sys
 
 def main():
+
+    # Initializes pygame and sets a clock as well as delta time 
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    # Creates two groups to hold multiple game objects, in this case, updatable and drawable objects
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    # Create an asteroid group
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    
+    # Assign the two groups as containers for all future player objects. All player objects will be updatable and drawable
+    Player.containers = (updatable, drawable)
+    # Assign groups for Asteroid objects and AsteroidField
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (updatable, drawable, shots)
+    # Creates a screen based on SCREEN_WIDTH and SCREEN_HEIGHT that was set in constants.py
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroid_field = AsteroidField()
+
     while True:
+
+        # Quits the game if the window is closed
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
+        # Sets the screen to black, can be RGB or string "black"
+        #screen.fill((0, 0, 0))
+        screen.fill("black")
+        
+
+        # Updates all objects within the updatable group
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                print("Game Over!")
+                sys.exit()
+            for bullet in shots:
+                if asteroid.collision(bullet):
+                    asteroid.split()
+                    bullet.kill()
+        # Loops through each object in the drawable group and draws them
+        for objects in drawable:
+            objects.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
